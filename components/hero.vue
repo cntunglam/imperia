874061eaa32c4757d46b232936a4b2276de05a57
Lam Cao <caongoctunglam@gmail.com>
2023-10-30 16:55:34 +0700

    change video link

--- components/hero.vue
+++ components/hero.vue
@@ -1,5 +1,5 @@
 <template>
-    <header class="relative z-0 flex items-center md:h-[600px] w-full aspect-square md:aspect-video justify-center mb-12 overflow-hidden">
+    <header class="relative z-0 flex items-center md:h-[600px] w-full aspect-square md:aspect-video justify-center overflow-hidden">
         <video
             autoplay
             loop
@@ -7,7 +7,7 @@
             class="absolute z-10 w-auto min-w-full min-h-full max-w-none"
         >
             <source
-            src="/video/video-hero.mov"
+            src="https://imperia.store/cdn/shop/t/26/assets/imperia_hardware_wide.mov?v=167117665659712184821660065711"
             type="video/mp4"
             />
             Your browser does not support the video tag.
