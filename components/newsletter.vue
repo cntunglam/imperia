259962388bc6349914c3627d594abacfc75307b5
Lam Cao <caongoctunglam@gmail.com>
2023-12-07 19:22:50 +0700

    text overhang fixed

--- components/newsletter.vue
+++ components/newsletter.vue
@@ -1,17 +1,17 @@
 <template>
-    <div class="px-4 w-full md:w-2/3 mx-auto">
+    <div class="px-4 w-full max-w-5xl mx-auto">
         <div class="py-4">
             <h2 class="text-[22px] md:text-[36px] text-center uppercase py-2">sign up for exclusive access</h2>
             <p class="text-center text-[14px] uppercase">receive 10% off your first purchase plus communications about imperia products, services, stores and events.</p>
         </div>
-        <div class="form-control py-4 w-full max-w-4xl mx-auto space-y-6">
+        <div class="form-control py-4 w-full space-y-6">
             <div class="flex flex-col space-y-4 md:space-x-8 md:flex-row md:space-y-0">
-                <input type="text" placeholder="First Name" class="input uppercase input-bordered w-full max-w-md rounded-none  border-1 border-black focus:outline-none focus:border-black focus:ring-1 focus:ring-black" />
-                <input type="text" placeholder="Last Name" class="input uppercase input-bordered w-full max-w-md rounded-none  border-1 border-black focus:outline-none focus:border-black focus:ring-1 focus:ring-black" />
+                <input type="text" placeholder="First Name" class="w-full input uppercase input-bordered rounded-none  border-1 border-black focus:outline-none focus:border-black focus:ring-1 focus:ring-black" />
+                <input type="text" placeholder="Last Name" class="w-full input uppercase input-bordered rounded-none  border-1 border-black focus:outline-none focus:border-black focus:ring-1 focus:ring-black" />
             </div>
-            <div class="flex flex-col space-y-4 max-w-4xl md:space-x-8 md:flex-row md:space-y-0">
-                <input type="email" placeholder="Email Address" class="input uppercase input-bordered w-full max-w-md rounded-none border-1 border-black focus:outline-none focus:border-black focus:ring-1 focus:ring-black " />
-                <input type="tel" placeholder="Phone Number" class="input uppercase input-bordered w-full max-w-md rounded-none border-1 border-black focus:outline-none focus:border-black focus:ring-1 focus:ring-black " />
+            <div class="flex flex-col space-y-4 md:space-x-8 md:flex-row md:space-y-0">
+                <input type="email" placeholder="Email Address" class="w-full input uppercase input-bordered rounded-none border-1 border-black focus:outline-none focus:border-black focus:ring-1 focus:ring-black " />
+                <input type="tel" placeholder="Phone Number" class="w-full input uppercase input-bordered rounded-none border-1 border-black focus:outline-none focus:border-black focus:ring-1 focus:ring-black " />
             </div>
             <p class="text-sm uppercase">By clicking <strong>SIGN UP</strong> , you agree to receive marketing communications from <strong>imperia</strong>  at the number and email address provided. Consent is not a condition of any purchase. message and data rates may apply. message frequency varies. view our <a href="/"  class="underline">privacy policy</a>  and <a href="/" class="underline">terms of service</a> </p>
             <button class="btn btn-primary my-4">Sign Up</button>
