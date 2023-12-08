86298026d29d2daf4b31dd8e0e35ff448dea319d
Lam Cao <caongoctunglam@gmail.com>
2023-12-08 09:03:39 +0700

    typo test

--- components/newsletter.vue
+++ components/newsletter.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="px-4 w-full max-w-5xl mx-auto">
         <div class="py-4">
-            <h2 class="text-[22px] md:text-[36px] text-center uppercase py-4 md:px-5">sign up for exclusive access</h2>
-            <p class="text-center text-[14px] uppercase">receive 10% off your first purchase plus communications about imperia products, services, stores and events.</p>
+            <h2 class="text-[23px] md:text-[36px] leading-8 text-center uppercase py-4 md:px-5">sign up for exclusive access</h2>
+            <p class="text-center text-[12px] uppercase">receive 10% off your first purchase plus communications about imperia products, services, stores and events.</p>
         </div>
         <div class="form-control py-4 w-full space-y-6">
             <div class="flex flex-col space-y-4 md:space-x-8 md:flex-row md:space-y-0">
@@ -13,7 +13,7 @@
                 <input type="email" placeholder="Email Address" class="w-full input uppercase input-bordered rounded-none border-1 border-black focus:outline-none focus:border-black focus:ring-1 focus:ring-black " />
                 <input type="tel" placeholder="Phone Number" class="w-full input uppercase input-bordered rounded-none border-1 border-black focus:outline-none focus:border-black focus:ring-1 focus:ring-black " />
             </div>
-            <p class="text-sm uppercase">By clicking <strong>SIGN UP</strong> , you agree to receive marketing communications from <strong>imperia</strong>  at the number and email address provided. Consent is not a condition of any purchase. message and data rates may apply. message frequency varies. view our <a href="/"  class="underline">privacy policy</a>  and <a href="/" class="underline">terms of service</a> </p>
+            <p class="text-sm uppercase">By clicking <strong>SIGN UP</strong>, you agree to receive marketing communications from <strong>imperia</strong>  at the number and email address provided. Consent is not a condition of any purchase. message and data rates may apply. message frequency varies. view our <a href="/"  class="underline">privacy policy</a>  and <a href="/" class="underline">terms of service</a> </p>
             <button class="btn btn-primary my-4">Sign Up</button>
         </div>
     </div>
