5bf0b7d9d642434513f68c96a19ad05af046a6c2
Lam Cao <caongoctunglam@gmail.com>
2024-01-29 19:47:42 +0700

    fixed

--- components/newsletter.vue
+++ components/newsletter.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="px-4 w-full max-w-5xl mx-auto">
         <div class="space-y-2">
-            <h2 class="text-[28px] md:text-[28px] leading-8 text-center uppercase md:px-5">sign up for exclusive access</h2>
+            <h2 class="text-[23px] md:text-[28px] leading-8 text-center uppercase md:px-5">sign up for exclusive access</h2>
             <p class="text-center text-[12px] uppercase">receive 10% off your first purchase plus communications about imperia products, services, stores and events.</p>
         </div>
         <div class="form-control py-4 w-full space-y-4 md:space-y-6">
