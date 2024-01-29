8c35999ae98842ed99b58a5c70cddab6aae55e2f
Lam Cao <caongoctunglam@gmail.com>
2024-01-29 19:40:22 +0700

    fixed

--- components/newsletter.vue
+++ components/newsletter.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="px-4 w-full max-w-5xl mx-auto">
         <div class="space-y-2">
-            <h2 class="text-[23px] md:text-[36px] leading-8 text-center uppercase md:px-5">sign up for exclusive access</h2>
+            <h2 class="text-[28px] md:text-[28px] leading-8 text-center uppercase md:px-5">sign up for exclusive access</h2>
             <p class="text-center text-[12px] uppercase">receive 10% off your first purchase plus communications about imperia products, services, stores and events.</p>
         </div>
         <div class="form-control py-4 w-full space-y-4 md:space-y-6">
@@ -13,7 +13,7 @@
                 <input type="email" placeholder="Email Address" class="w-full input uppercase input-bordered rounded-none border-1 border-black focus:outline-none focus:border-gray-200 focus:ring-1 focus:ring-gray-600 " />
                 <input type="tel" placeholder="Phone Number" class="w-full input uppercase input-bordered rounded-none border-1 border-black focus:outline-none focus:border-gray-200 focus:ring-1 focus:ring-gray-600 " />
             </div>
-            <p class="text-sm uppercase">By clicking <strong>SIGN UP</strong>, you agree to receive marketing communications from <strong>imperia</strong>  at the number and email address provided. Consent is not a condition of any purchase. message and data rates may apply. message frequency varies. view our <a href="/"  class="underline">privacy policy</a>  and <a href="/" class="underline">terms of service</a> </p>
+            <p class="text-[12px] uppercase">By clicking <strong>SIGN UP</strong>, you agree to receive marketing communications from <strong>imperia</strong>  at the number and email address provided. Consent is not a condition of any purchase. message and data rates may apply. message frequency varies. view our <a href="/"  class="underline">privacy policy</a>  and <a href="/" class="underline">terms of service</a> </p>
             <button class="btn btn-primary my-4 font-normal rounded-none">Sign Up</button>
         </div>
     </div>
