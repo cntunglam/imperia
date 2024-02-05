8c5deb52bc6275db0fca1ba588d837aef37305ae
Lam Cao <caongoctunglam@gmail.com>
2024-02-05 19:25:17 +0700

    disable zoom

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,7 +15,7 @@ export default defineNuxtConfig({
   app: {
     head: {
       charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1',
+      viewport: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no',
     }
   },
   css: ['~/assets/css/main.css', '~/assets/fonts/stylesheet.css'],
