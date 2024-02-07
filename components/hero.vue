509432b0e21c60ac8102aae10754d2e20ac487e2
Lam Cao <caongoctunglam@gmail.com>
2024-02-07 11:37:40 +0700

    video upload

--- components/hero.vue
+++ components/hero.vue
@@ -1,7 +1,7 @@
 <template>
         <section class="relative h-screen flex flex-col items-center justify-center text-center text-white py-0 px-3 z-0">
             <div class="video-docker absolute top-0 left-0 w-full min-h-full overflow-hidden">
-                <video class="min-w-full min-h-full absolute object-cover" src="https://imperia.store/cdn/shop/t/26/assets/imperia_hardware_wide.mov" type="video/mp4" playsinline autoplay muted loop></video>
+                <video class="min-w-full min-h-full absolute object-cover" src="video/welcome-video.mp4" type="video/mp4" playsinline autoplay muted loop></video>
             </div>
         </section>
 </template> 
