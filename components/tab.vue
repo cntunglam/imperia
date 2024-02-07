cf682aa364fa22ad09658376ea638505ee11b0cd
Lam Cao <caongoctunglam@gmail.com>
2024-02-07 12:08:27 +0700

    tab fixed

--- components/tab.vue
+++ components/tab.vue
@@ -2,10 +2,10 @@
   <div
     role="tablist"
     class="tabs py-2 tabs-boxed space-x-2 flex flex-nowrap overflow-y-hidden overflow-x-scroll max-w-full no-scrollbar justify-start md:justify-center px-2 md:px-0 bg-transparent uppercase font-sans">
-        <a role="tab" class="tab tab-sm">T-Shirts</a>
-        <a role="tab" class="tab tab-sm">Hoodies</a>
-        <a role="tab" class="tab tab-sm">Shorts</a>
-        <a role="tab" class="tab tab-sm">Luxury</a>
-        <a role="tab" class="tab tab-sm">Accessories</a>
+        <a role="tab" class="tab tab-sm text-[11px]">T-Shirts</a>
+        <a role="tab" class="tab tab-sm text-[11px]">Hoodies</a>
+        <a role="tab" class="tab tab-sm text-[11px]">Shorts</a>
+        <a role="tab" class="tab tab-sm text-[11px]">Luxury</a>
+        <a role="tab" class="tab tab-sm text-[11px]">Accessories</a>
     </div>
 </template>
