a7be6b6654d03b99cae7243164892ba3b5272b84
Lam Cao <caongoctunglam@gmail.com>
2024-02-24 14:23:00 +0700

    align login

--- pages/login.vue
+++ pages/login.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="grid py-12">
+  <div class="grid">
     <div class="px-2 ">
       <h2 class="text-[28px] py-8 uppercase text-center">Login</h2>
       <div class="form-control py-2 w-full max-w-lg mx-auto uppercase">
@@ -14,7 +14,7 @@
           placeholder="First Name"
           class="w-full md:text-[10px] text-[12px] input uppercase input-bordered border-1 border-black focus:outline-none focus:border-gray-200 focus:ring-1 focus:ring-gray-600"
         />
-        <div class="flex justify-between">
+        <div class="flex justify-between py-2">
           <div>
             <input type="checkbox" id="remember" class="align-middle appearance-none w-4 h-4 border border-black rounded-none checked:text-black"/>
             <label for="remember" class="text-[12px] px-2">Remember me</label>
@@ -24,12 +24,12 @@
           </div>
         </div>
         </div>
-        <button class="btn btn-primary my-4 text-[10px]">Login</button>
-        <p class="text-center underline"><a href="">Create An Account</a></p>
-        <hr class="border-black mt-8"/>
+        <button class="btn btn-primary text-[10px]">Login</button>
+        <p class="text-center underline py-8"><a href="">Create An Account</a></p>
+        <hr class="border-black"/>
       </div>
     </div>
-    <div class="text-[12px] max-w-xl mx-auto space-y-4">
+    <div class="text-[12px] max-w-xl mx-auto space-y-4 py-8">
       <p>ENJOY AN ELEVATED SHOPPING EXPERIENCE WITH YOUR EXCLUSIVE ACCESS</p>
       <ul class="space-y-2">
         <li>- CHECK THE DETAILS AND MONITOR THE STATUS OF YOUR ORDERS</li>
