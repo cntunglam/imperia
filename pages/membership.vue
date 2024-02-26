92f8612c17cfdd57e95816967f114f1ba71fa3fd
Lam Cao <caongoctunglam@gmail.com>
2024-02-26 16:14:53 +0700

    membership changes

--- pages/membership.vue
+++ pages/membership.vue
@@ -7,15 +7,15 @@
             role="tablist"
             class="tabs space-x-2 flex flex-nowrap overflow-y-hidden overflow-x-scroll max-w-full no-scrollbar justify-start md:justify-center px-2 md:px-0 bg-transparent uppercase font-sans"
         >
-            <a role="tab" class="tab tab-xl text-black">My Account</a>
-            <a role="tab" class="tab tab-xl text-black">Orders</a>
-            <a role="tab" class="tab tab-xl text-black">Profile </a>
-            <a role="tab" class="tab tab-xl text-black">Log Out</a>
+            <a role="tab" class="tab tab-xl text-[14px] text-black">My Account</a>
+            <a role="tab" class="tab tab-xl text-[14px] text-black">Orders</a>
+            <a role="tab" class="tab tab-xl text-[14px] text-black">Profile </a>
+            <a role="tab" class="tab tab-xl text-[14px] text-black">Log Out</a>
         </div>
         <div class="px-2 grid h-52">
             <hr class="border-t-[1px] border-black">
-            <p class="place-self-center">You have no placed order</p>
-            <button class="btn btn-primary text-[10px] w-full mt-4 place-self-end">Shop New Arrivals</button>
+            <p class="place-self-center text-[12px]">You have no placed order</p>
+            <button class="btn btn-primary text-[10px] w-full place-self-end">Shop New Arrivals</button>
         </div>
 
     </div>
