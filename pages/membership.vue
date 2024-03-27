73df1455ccce29a9733663e4ba9c33a2d085c9e4
Dat Nguyen <wildtigon@gmail.com>
2024-03-27 23:33:49 +0700

    fixed font size

--- pages/membership.vue
+++ pages/membership.vue
@@ -1,3 +1,6 @@
+<script setup>
+    
+</script>
 <template>
     <div class="_body space-y-8">
         <div>
@@ -7,16 +10,16 @@
             role="tablist"
             class="tabs flex max-w-full justify-center md:space-x-4 md:px-0 bg-transparent uppercase font-sans"
         >
-            <a role="tab" class="tab tab-xl text-[14px] text-black">My Account</a>
-            <a role="tab" class="tab tab-xl text-[14px] text-black underline">Orders</a>
-            <a role="tab" class="tab tab-xl text-[14px] text-black">Profile </a>
-            <a role="tab" class="tab tab-xl text-[14px] text-black">Log Out</a>
+            <a role="tab" class="tab tab-xl text-black">My Account</a>
+            <a role="tab" class="tab tab-xl text-black underline">Orders</a>
+            <a role="tab" class="tab tab-xl text-black">Profile </a>
+            <a role="tab" class="tab tab-xl text-black">Log Out</a>
         </div>
         <div class="grid h-64">
             <hr class="border-t-[1px] border-black">
             <p class="place-self-center text-[12px]">You have no placed order</p>
             <button class="btn btn-primary w-full place-self-end">Shop New Arrivals</button>
         </div>
-
     </div>
+
 </template>
