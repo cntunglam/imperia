244759e99e17bd3388d8968967e41ef4bf81796d
Dat Nguyen <wildtigon@gmail.com>
2024-04-11 08:05:52 +0700

    styleWidth changed

--- components/styleWith.vue
+++ components/styleWith.vue
@@ -3,7 +3,7 @@ import { product } from "/assets/product.json";
 </script>
 <template>
 <section class="mx-auto pt-14 px-4 ">
-    <h2 class="md:text-5xl text-4xl text-center uppercase">Style With</h2>
+    <h2 class="text-[20px] text-center uppercase">Style With</h2>
     <div class="grid md:grid-cols-4 grid-cols-2 px-2 py-10 gap-2 md:gap-8">
       <div v-for="item in product">
         <ProductCard
