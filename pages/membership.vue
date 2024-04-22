d30dcc0403560ea062e2eeb65c42e64a3c44600a
Dat Nguyen <wildtigon@gmail.com>
2024-04-22 15:52:25 +0700

    date picker fixed

--- pages/membership.vue
+++ pages/membership.vue
@@ -200,11 +200,8 @@
                         placeholder="Email Address"
                         class="w-full md:text-[10px] text-[12px] input uppercase input-bordered border-1 border-black focus:outline-none focus:border-gray-200 focus:ring-1 focus:ring-gray-600"
                         />
-                        <input
-                            type="date"
-                            placeholder="Date of Birth"
-                            class="w-full md:text-[10px] text-[12px] input uppercase input-bordered border-1 border-black focus:outline-none focus:border-gray-200 focus:ring-1 focus:ring-gray-600"
-                        />
+                        <input type="date" placeholder="dd/mm/yyyy" pattern="(^(((0[1-9]|1[0-9]|2[0-8])[\/](0[1-9]|1[012]))|((29|30|31)[\/](0[13578]|1[02]))|((29|30)[\/](0[4,6,9]|11)))[\/](19|[2-9][0-9])\d\d$)|(^29[\/]02[\/](19|[2-9][0-9])(00|04|08|12|16|20|24|28|32|36|40|44|48|52|56|60|64|68|72|76|80|84|88|92|96)$)"                         class="w-full md:text-[10px] text-[12px] input uppercase input-bordered border-1 border-black focus:outline-none focus:border-gray-200 focus:ring-1 focus:ring-gray-600"
+                         required>
                         <div class="flex gap-2">
                             <input
                             type="number"
