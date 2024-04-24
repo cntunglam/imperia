90a28b10ecae28347feeeac0b5530e83c93db530
Dat Nguyen <wildtigon@gmail.com>
2024-04-24 10:36:44 +0700

    fixed

--- components/date-picker.vue
+++ components/date-picker.vue
@@ -4,9 +4,9 @@
         type="date"
         class="w-full custom-date-input md:text-[10px] text-[12px] input uppercase input-bordered border-1 border-black focus:outline-none focus:border-gray-200 focus:ring-1 focus:ring-gray-600"
         v-model="selectedDate"
-        placeholder="Select a date"
         @input="updatePlaceholderVisibility"
       />
+      <span v-if="isIPhone && !selectedDate" class="placeholder">Select a date</span>
     </div>
   </template>
   
@@ -14,13 +14,17 @@
   export default {
     data() {
       return {
-        selectedDate: ''
+        selectedDate: '',
+        isIPhone: false
       };
+    },
+    mounted() {
+      // Detect if the user agent is an iPhone
+      this.isIPhone = /(iPhone|iPod)/.test(navigator.userAgent);
     },
     methods: {
       updatePlaceholderVisibility() {
-        // This method can be removed
-        // No need for custom logic since native placeholder behavior is used
+        // This method ensures the placeholder is shown when the input is empty
       }
     }
   };
@@ -35,6 +39,15 @@
     appearance: none;
     -webkit-appearance: none;
     -moz-appearance: none;
+  }
+  
+  .placeholder {
+    position: absolute;
+    left: 10px;
+    top: 50%;
+    transform: translateY(-50%);
+    color: #999; /* Set the color of the placeholder */
+    pointer-events: none; /* Ensure the placeholder text doesn't interfere with input */
   }
   </style>
   
