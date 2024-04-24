4ffef0d0575f1018c9dac771dbe478571db8ec55
Dat Nguyen <wildtigon@gmail.com>
2024-04-24 10:43:33 +0700

    fixed

--- components/date-picker.vue
+++ components/date-picker.vue
@@ -2,7 +2,7 @@
     <div class="custom-date-input">
       <input
         type="date"
-        class="w-full custom-date-input md:text-[10px] text-[12px] input uppercase input-bordered border-1 border-black focus:outline-none focus:border-gray-200 focus:ring-1 focus:ring-gray-600"
+        class="w-full custom-date-input md:text-[10px] text-[12px] align-left input uppercase input-bordered border-1 border-black focus:outline-none focus:border-gray-200 focus:ring-1 focus:ring-gray-600"
         v-model="selectedDate"
         @input="updatePlaceholderVisibility"
       />
