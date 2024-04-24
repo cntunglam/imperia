ceb11af3ebb3f4a72aa66f826cdf3a6b9361d83d
Dat Nguyen <wildtigon@gmail.com>
2024-04-24 10:29:55 +0700

    date picker fixed

--- components/date-picker.vue
+++ components/date-picker.vue
@@ -1,3 +1,39 @@
 <template>
-    <input type="date" id="myDate" class="w-full custom-date md:text-[10px] text-[12px] input uppercase input-bordered border-1 border-black focus:outline-none focus:border-gray-200 focus:ring-1 focus:ring-gray-600"/>
-  </template>
+  <div class="custom-date-input">
+    <input
+      type="date"
+      class="w-full md:text-[10px] text-[12px] input uppercase input-bordered border-1 border-black focus:outline-none focus:border-gray-200 focus:ring-1 focus:ring-gray-600"
+      v-model="selectedDate"
+      placeholder="Select a date"
+      @input="updatePlaceholderVisibility"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      selectedDate: ''
+    };
+  },
+  methods: {
+    updatePlaceholderVisibility() {
+      // This method can be removed
+      // No need for custom logic since native placeholder behavior is used
+    }
+  }
+};
+</script>
+
+<style scoped>
+.custom-date-input {
+  position: relative;
+}
+
+.input {
+  appearance: none;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+}
+</style>
