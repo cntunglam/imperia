6ed29298190c2e550c28eca09443a2b981738303
Dat Nguyen <wildtigon@gmail.com>
2024-04-24 10:33:07 +0700

    fixed

--- components/date-picker.vue
+++ components/date-picker.vue
@@ -1,39 +1,40 @@
 <template>
-  <div class="custom-date-input">
-    <input
-      type="date"
-      class="w-full md:text-[10px] text-[12px] input uppercase input-bordered border-1 border-black focus:outline-none focus:border-gray-200 focus:ring-1 focus:ring-gray-600"
-      v-model="selectedDate"
-      placeholder="Select a date"
-      @input="updatePlaceholderVisibility"
-    />
-  </div>
-</template>
-
-<script>
-export default {
-  data() {
-    return {
-      selectedDate: ''
-    };
-  },
-  methods: {
-    updatePlaceholderVisibility() {
-      // This method can be removed
-      // No need for custom logic since native placeholder behavior is used
+    <div class="custom-date-input">
+      <input
+        type="date"
+        class="w-full custom-date-input md:text-[10px] text-[12px] input uppercase input-bordered border-1 border-black focus:outline-none focus:border-gray-200 focus:ring-1 focus:ring-gray-600"
+        v-model="selectedDate"
+        placeholder="Select a date"
+        @input="updatePlaceholderVisibility"
+      />
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        selectedDate: ''
+      };
+    },
+    methods: {
+      updatePlaceholderVisibility() {
+        // This method can be removed
+        // No need for custom logic since native placeholder behavior is used
+      }
     }
+  };
+  </script>
+  
+  <style scoped>
+  .custom-date-input {
+    position: relative;
   }
-};
-</script>
-
-<style scoped>
-.custom-date-input {
-  position: relative;
-}
-
-.input {
-  appearance: none;
-  -webkit-appearance: none;
-  -moz-appearance: none;
-}
-</style>
+  
+  .input {
+    appearance: none;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+  }
+  </style>
+  
