ebac7c9c613303c728841b0859a9dd94fbad9b48
Dat Nguyen <wildtigon@gmail.com>
2024-05-31 21:36:19 +0700

    loop collection

--- components/styleWith.vue
+++ components/styleWith.vue
@@ -5,7 +5,7 @@ import { product } from "/assets/product.json";
 <section class="mx-auto pt-14 px-4 ">
     <h2 class="text-[20px] text-center uppercase">Style With</h2>
     <div class="grid md:grid-cols-4 grid-cols-2 px-2 py-10 gap-2 md:gap-8">
-      <div v-for="item in product">
+      <div v-for="item in product" :key="item">
         <ProductCard
           :link="item.permalink"
           :inStock="item.inStock"
@@ -16,6 +16,4 @@ import { product } from "/assets/product.json";
       </div>
     </div>
   </section>
-
-
 </template>
---
+++ pages/collection/index.vue
@@ -0,0 +1,45 @@
+<script setup>
+useSeoMeta({
+  title: "Collection - Imperia",
+});
+
+import { GET_PRODUCTS } from "~/queries/getProduct";
+
+// Fetch products data
+const { $shopifyClient } = useNuxtApp();
+const loading = ref(true);
+const products = ref([]);
+onMounted(async () => {
+  try {
+    const {data} = await $shopifyClient.request(GET_PRODUCTS,{
+          variables: {
+            number: 20,
+          }
+    });
+    products.value = data.products.edges.map(edge => edge.node);
+  } catch (error) {
+    console.error(error);
+  } finally {
+    loading.value = false;
+  }
+});
+
+</script>
+<template>
+  <div class="grid px-4 md:px-8">
+    <div class="">
+      <div class="grid md:grid-cols-4 grid-cols-2 px-2 md:py-10 gap-2 md:gap-8">
+      <div v-for="item in products" :key="item.id">
+        <ProductCard
+          :link="`/product/${item.handle}`"
+          :inStock="item.availableForSale"
+          :productName="item.title"
+          :productColor="item.variants.nodes[0]?.selectedOptions[0]?.value"
+          :Image="item.images.edges[0]?.node.src"
+          :price="item.variants.nodes[0]?.price.amount"
+        />
+      </div>
+      </div>
+    </div>
+  </div>
+</template>
