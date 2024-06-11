60bfd8872c29c08f0267746d4376a50b13254cbe
Dat Nguyen <wildtigon@gmail.com>
2024-06-11 13:29:08 +0700

    client service/faq

--- pages/page/client-service.vue
+++ pages/page/client-service.vue
@@ -1,30 +1,65 @@
+<script setup>
+import { GET_METAOBJECT } from "~/queries/getMetaobject";
+import { convertSchemaToHtml } from "@thebeyondgroup/shopify-rich-text-renderer";
+
+const { $shopifyClient } = useNuxtApp();
+const loading = ref(true);
+const content = ref([]);
+const openTime = ref("")
+
+onMounted(async () => {
+  try {
+    const { data } = await $shopifyClient.request(GET_METAOBJECT, {
+      variables: {
+        handle: "client-service",
+        type: "client_service",
+      },
+    });
+    content.value = data.metaobject.fields;
+    openTime.value = convertSchemaToHtml(content.value[6].value)
+  } catch (error) {
+    console.log(error);
+  } finally {
+    loading.value = false;
+  }
+});
+</script>
 <template>
-<div class="_body space-y-8">
+  <div class="_body space-y-8">
     <div>
-        <h2 class="text-[20px] uppercase text-center">Client Services</h2>
+      <h2 class="text-[20px] uppercase text-center">Client Services</h2>
     </div>
     <div class="mx-auto text-center">
-        <p class="uppercase font-[12px]">
-            OUR CLIENT SERVICE IS AVAILABLE TO ASSIST YOU WITH YOUR ONLINE ORDER. BY CONTACTING OUR CLIENT SERVICE, YOU CONSENT TO YOUR DATA BEING TRANSFERRED OUTSIDE OF YOUR LOCAL COUNTRY/REGION.
-        </p>
+      <p class="uppercase font-[12px]">
+        {{ content[1]?.value }}
+    </p>
     </div>
     <div class="mx-auto w-full text-center space-y-4">
-        <div>
-        <p class=" text-[12px] font-thin">Frequently Ask Questions</p>
-        <NuxtLink to="/page/faq" class="btn btn-primary text-[10px] w-full mt-4">See FAQ</NuxtLink>
+      <div>
+        <p class="text-[12px] font-thin">{{ content[4]?.value }}</p>
+        <NuxtLink
+          :to="content[5]?.value"
+          class="btn btn-primary text-[10px] w-full mt-4"
+          >See FAQ</NuxtLink
+        >
+      </div>
+      <div>
+        <p class="text-[12px] font-thin">{{ content[0]?.value }}</p>
+        <div v-html="openTime"></div>
+        <NuxtLink
+          :to="`tel:${content[7]?.value}`"
+          class="btn btn-primary text-[10px] w-full mt-4"
+          >+1 (678) 782 8404</NuxtLink
+        >
+      </div>
+      <div class="pt-8">
+        <p class="text-[12px] font-thin">{{ content[3]?.value }}</p>
+        <NuxtLink
+          :to="`mailto:${content[2]?.value}`"
+          class="btn btn-primary text-[10px] w-full mt-4"
+          >Send A Message</NuxtLink
+        >
+      </div>
     </div>
-    <div>
-        <p class=" text-[12px] font-thin">Call Us</p>
-        <p>Mon - Fri 9AM - 9PM EST</p>
-        <p>Sat - Sun 10AM - 7PM EST</p>
-        <NuxtLink to="tel:+16787828404" class="btn btn-primary text-[10px] w-full mt-4">+1 (678) 782 8404</NuxtLink>
-    </div>
-    <div class="pt-8">
-        <p class=" text-[12px] font-thin">Email Us</p>
-        <NuxtLink to="mailto:NjLbE@example.com" class="btn btn-primary text-[10px] w-full mt-4">Send A Message</NuxtLink>
-    </div>
-
-    </div>
-</div>
-
+  </div>
 </template>
--- pages/page/faq.vue
+++ pages/page/faq.vue
@@ -1,67 +1,55 @@
+<script setup>
+import { GET_METAOBJECTS } from "~/queries/getMetaobject";
+import { convertSchemaToHtml } from "@thebeyondgroup/shopify-rich-text-renderer";
+
+const { $shopifyClient } = useNuxtApp();
+const loading = ref(true);
+const content = ref([]);
+onMounted(async () => {
+  try {
+    const { data } = await $shopifyClient.request(GET_METAOBJECTS, {
+      variables: {
+        type: "faq",
+      },
+    });
+    content.value = data.metaobjects.nodes;
+  } catch (error) {
+    console.log(error);
+  } finally {
+    loading.value = false;
+  }
+});
+</script>
 <template>
-    <main class="_body space-y-12">
-      <div class="text-center space-y-8">
-        <h1 class="text-[20px]">FAQ</h1>
-        <p class="text-[12px]">SELECT A CATEGORY BELOW TO SEE ANSWERS TO FREQUENTLY ASKED QUESTIONS</p>
-        <select
-            class="w-full select select-bordered uppercase border-black rounded-none"
-          >
-            <option>Order And Payment</option>
-            <option>Lorem Ipsum</option>
-            <option>Lorem Ipsum</option>
-            <option>Lorem Ipsum</option>
-            <option>Lorem Ipsum</option>
-          </select>
-      </div>
-      <div class="_accordion mx-auto my-4 text-[12px]">
-            <details class="collapse collapse-arrow uppercase">
-            <summary class="collapse-title text-[12px] font-medium">WHAT FORMS OF PAYMENT DO YOU ACCEPT?</summary>
-            <div class="collapse-content text-[12px]"> 
-              <p>content</p>
-            </div>
-            </details>
-            <details class="collapse collapse-arrow uppercase">
-            <summary class="collapse-title text-[12px] font-medium">WHY CAN'T I COMPLETE MY ORDER?</summary>
-            <div class="collapse-content text-[12px]"> 
-              <p>content</p>
-            </div>
-            </details>
-            <details class="collapse collapse-arrow uppercase">
-            <summary class="collapse-title text-[12px] font-medium">CAN I CANCEL OR MODIFY MY ORDER?</summary>
-            <div class="collapse-content text-[12px]"> 
-              <p>content</p>
-            </div>
-            </details>
-            <details class="collapse collapse-arrow uppercase">
-            <summary class="collapse-title text-[12px] font-medium ">WHY IS THE AMOUNT I PAID DIFFERENT FROM THE AMOUNT OF MY ORDER?</summary>
-            <div class="collapse-content text-[12px]"> 
-                <p>content</p>
-            </div>
-            </details>
-            <details class="collapse collapse-arrow uppercase">
-            <summary class="collapse-title text-[12px] font-medium">WILL I BE CHARGED SALES TAX?</summary>
-            <div class="collapse-content text-[12px]"> 
-              <p>content</p>
-            </div>
-            </details>
-            <details class="collapse collapse-arrow uppercase">
-            <summary class="collapse-title text-[12px] font-medium">WHAT IS A PRE-ORDER ITEM?</summary>
-            <div class="collapse-content text-[12px]"> 
-              <p>content</p>
-            </div>
-            </details>
-            <details class="collapse collapse-arrow uppercase">
-            <summary class="collapse-title text-[12px] font-medium">CAN I GET MY PURCHASE GIFT WRAPPED?</summary>
-            <div class="collapse-content text-[12px]"> 
-              <p>content</p>
-            </div>
-            </details>
-            <details class="collapse collapse-arrow uppercase">
-            <summary class="collapse-title text-[12px] font-medium">WHERE CAN I PURCHASE IMPERIA?</summary>
-            <div class="collapse-content text-[12px]"> 
-              <p>content</p>
-            </div>
-            </details>
-          </div>
-    </main>
+  <main class="_body space-y-12">
+    <div class="text-center space-y-8">
+      <h1 class="text-[20px]">FAQ</h1>
+      <p class="text-[12px]">
+        SELECT A CATEGORY BELOW TO SEE ANSWERS TO FREQUENTLY ASKED QUESTIONS
+      </p>
+      <select
+        class="w-full select select-bordered uppercase border-black rounded-none"
+      >
+        <option>Order And Payment</option>
+        <option>Lorem Ipsum</option>
+        <option>Lorem Ipsum</option>
+        <option>Lorem Ipsum</option>
+        <option>Lorem Ipsum</option>
+      </select>
+    </div>
+    <div class="_accordion mx-auto my-4 text-[12px]">
+      <details
+        v-for="item in content"
+        :key="item"
+        class="collapse collapse-arrow uppercase"
+      >
+        <summary class="collapse-title text-[12px] font-medium">
+          {{ item.fields[1].value }}
+        </summary>
+        <div class="collapse-content text-[12px]">
+          <div v-html="convertSchemaToHtml(item.fields[0].value)"></div>
+        </div>
+      </details>
+    </div>
+  </main>
 </template>
