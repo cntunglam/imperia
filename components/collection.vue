f03cb1fd1fa38a846699377875e32149003646f9
Dat Nguyen <wildtigon@gmail.com>
2024-07-11 17:48:30 +0700

    fixed

--- components/collection.vue
+++ components/collection.vue
@@ -23,19 +23,20 @@ onMounted(async () => {
       class="w-full hover:filter hover:grayscale duration-100 flex justify-center bg-top lg:bg-center bg-cover object-center"
       :class="[
         index === 0 ? 'lg:row-span-2 lg:col-span-1 col-span-2 h-full' : '',
-        'bg-top lg:bg-center bg-cover'
+        'bg-top lg:bg-center bg-cover',
       ]"
-      :style="{ backgroundImage: `url(${collection.image?.src})`}"
+      :style="{ backgroundImage: `url(${collection.image?.src})` }"
+      @click="$router.push(`/collection/${collection.handle}`)"
     >
-      <a :href="`/collection/${collection.handle}`">
-      <div
-        class="w-full px-10 py-1 align- mx-auto justify-center self-end flex"
-      >
-        <a :href="`/collection/${collection.handle}`">
-          <h2 class="text-[14px] text-white py-1 uppercase w-full">{{ collection.title }}</h2>
-        </a>
-      </div>
-      </a>
+        <div
+          class="w-full px-10 py-1 align- mx-auto justify-center self-end flex"
+        >
+          <a :href="`/collection/${collection.handle}`">
+            <h2 class="text-[14px] text-white py-1 uppercase w-full">
+              {{ collection.title }}
+            </h2>
+          </a>
+        </div>
     </div>
   </div>
 </template>
